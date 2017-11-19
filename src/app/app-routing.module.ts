5c4058c81135e757307201372fde7581c25bd249
Peter Borkuti <peter@aspire>
2017-11-19 17:29:52 +0100

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlacementComponent } from './placement/placement.component';
+import { BattleComponent } from './battle/battle.component';
+import { ExitComponent } from './exit/exit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/placement', pathMatch: 'full' },
+  { path: 'placement', component: PlacementComponent },
+  { path: 'battle', component: BattleComponent },
+  { path: 'exit', component: ExitComponent }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 
