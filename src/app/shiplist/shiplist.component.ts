8da67e7bd744804f2166a87243c473382ba49495
Peter Borkuti <peter@aspire>
2017-11-22 21:02:24 +0100

    new images and ng-bootstrap carousel

--- src/app/shiplist/shiplist.component.ts
+++ src/app/shiplist/shiplist.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Ship } from '../ships/ship';
 import { ShipService } from '../ships/ship.service';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-shiplist',
   templateUrl: './shiplist.component.html',
-  styleUrls: ['./shiplist.component.css']
+  styleUrls: ['./shiplist.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class ShiplistComponent implements OnInit {
   ships: Ship[];
   active: number;
 
-  constructor(private shipService: ShipService) { }
+  constructor(private shipService: ShipService, config: NgbCarouselConfig) {
+    config.interval = 0;
+    config.wrap = true;
+    config.keyboard = false;
+
+   }
 
   ngOnInit() {
     this.shipService.getShips().subscribe(
@@ -22,8 +29,24 @@ export class ShiplistComponent implements OnInit {
             e['imagePath'] = 'assets/' + e.imageFileName; return e;
           }
         );
-      this.active = this.ships[0].id;
+      this.active = 0;
       }
     );
+  }
+
+  next() {
+    if (this.active < this.ships.length - 1) {
+      this.active++;
+    } else {
+      this.active = 0;
+    }
+  }
+
+  prev() {
+    if (this.active > 0) {
+      this.active--;
+    } else {
+      this.active = this.ships.length - 1;
+    }
   }
 }
